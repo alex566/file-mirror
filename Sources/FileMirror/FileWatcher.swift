228a92c483f9c7890c57047589ee676e6b9a2ef9
Oleksii Oliinyk <alexey566@icloud.com>
2025-04-20 00:40:09 +0200

    Add file watcher

---
+++ Sources/FileMirror/FileWatcher.swift
@@ -0,0 +1,55 @@
+import Foundation
+
+final class FileWatcher {
+    
+    enum Event {
+        case write
+        case delete
+    }
+    
+    enum WatchError: Error {
+        case failedToOpenFile(URL)
+    }
+    
+    func observe(url: URL, event: Event) -> AsyncThrowingStream<Void, Error> {
+        AsyncThrowingStream { continuation in
+            let fileDescriptor = open(url.path(percentEncoded: false), O_EVTONLY)
+            guard fileDescriptor >= 0 else {
+                continuation.finish(throwing: WatchError.failedToOpenFile(url))
+                return
+            }
+            
+            let eventMask: DispatchSource.FileSystemEvent = switch event {
+                case .write:
+                    .write
+                case .delete:
+                    .delete
+            }
+            
+            // Create a dispatch source to monitor the file descriptor for events
+            let dispatchSource = DispatchSource.makeFileSystemObjectSource(
+                fileDescriptor: fileDescriptor,
+                eventMask: eventMask,
+                queue: .main
+            )
+
+            // Set the event handler to respond to changes
+            dispatchSource.setEventHandler {
+                continuation.yield()
+            }
+
+            // Set a cancel handler to close the file descriptor when monitoring stops
+            dispatchSource.setCancelHandler {
+                close(fileDescriptor)
+            }
+
+            // Start monitoring
+            dispatchSource.resume()
+            
+            // Set up cancellation to properly clean up resources
+            continuation.onTermination = { _ in
+                dispatchSource.cancel()
+            }
+        }
+    }
+}
